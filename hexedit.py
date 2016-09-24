c3cd6ee959bb5b4833b98638eb42aa66bbdb5c63
Michael Trittin <Michael Trittin>
2016-09-23 21:02:45 -0400

    Fixed a bug with odd-length strings

--- hexedit.py
+++ hexedit.py
@@ -3,5 +3,7 @@ import sys
 if (len(sys.argv) < 2):
 	print("Invalid arguments")
 	print("Provide hex to convert to ascii")
-else:
+elif (len(sys.argv[1])%2 == 0):
 	sys.stdout.write(sys.argv[1].decode('hex'))
+else:
+	sys.stdout.write((sys.argv[1]+"0").decode('hex'))
